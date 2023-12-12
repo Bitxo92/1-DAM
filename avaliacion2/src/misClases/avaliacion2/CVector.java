7dbfe7b6d184f860bfe1c85aef5708aff55fdf49
Alejandro Patiño <alexpatino92@yahoo.com>
2023-12-12 11:03:44 +0100

    mod

--- avaliacion2/src/misClases/avaliacion2/CVector.java
+++ avaliacion2/src/misClases/avaliacion2/CVector.java
@@ -10,50 +10,67 @@ public class CVector {
 	private Scanner scanner = new Scanner(System.in);
 
 	public void inicializaVector() {
-		
-		vector[0]= 3;
-		vector[1]= 12;
-		vector[2]= 15;
-		vector[3]= 22;
-		vector[4]= 30;
-		vector[5]= 43;
-		
-      total=vector.length;  
-    }
+
+		vector[0] = 3;
+		vector[1] = 12;
+		vector[2] = 15;
+		vector[3] = 22;
+		vector[4] = 30;
+		vector[5] = 43;
+
+		total = 6;
+	}
 
 	public void mostrarVector() {
-		total=0;
-		for (int i : vector) {
-			if (i != 0) {
-				System.out.print( i + " ");
-				total++;
-				
-			}
-			
-		}
-		System.out.println("total: "+total);
-		
+
+		for (int i = 0; i < total; i++)
+			System.out.println("el valor de " + i + ": " + vector[i]);
+
+		System.out.println("total: " + total);
 
 	}
-	
+
 	public void eliminaVector(int a) {
-		for (int i=0;i<vector.length;i++) {
-			if(a==vector[i]) 
-				vector[i]=0;
-				
-			
-			
+		int posicion=0;
+		for (int i = 0; i < total; i++) {
+			if (vector[i]==a) {
+				vector[i] = 0;
+				posicion=i;
+			}
+
 		}
 		
+		for(int i=posicion+1;i<total;i++) {
+			vector[i-1]=vector[i];
+		}
+		
+		total--;
+
 	}
-	
-	
+
 	public void insertaVector(int a) {
-		int i=total;
-		vector[i]=a;
+		int posicion=0;
+		//for-loop para buscar la posicion para el numero introducido
+		for(int i=0;i<total;i++) {
+			if(vector[i]<a) {
+				posicion=i+1;
+				
+				
+			}
+		}
+		//desplazamos valores en el array para hacer sitio para el nuevo valor
+		
+		for(int i=total;i>posicion;i--) {
+			vector[i]=vector[i-1];
+			
+		}
+		//introducimos valor
+		vector[posicion]=a;
 		total++;
+		
+		
 	}
-	
+
 	private void mostrarMenu() {
 		System.out.println("\n");
 		System.out.println("MENU");
@@ -64,7 +81,7 @@ public class CVector {
 		System.out.println("4.- ELIMINA enteiros en Vector");
 		System.out.println("5.- FINAL");
 	}
-	
+
 	public int recolleOpcion() {
 		mostrarMenu();
 		System.out.println("Introduzca una opción: ");
